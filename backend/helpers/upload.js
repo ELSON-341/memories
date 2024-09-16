7dc6ebe59447d57a4db94de7bbad8a2bebf806d9
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-09-16 14:29:01 -0300

    multer

---
+++ backend/helpers/upload.js
@@ -0,0 +1,26 @@
+const multer = require("multer")
+const path = require('path')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cd) {
+        cd(null, path.join(__dirname, "../pubric/images/"))
+    },
+    filename: function(req, file, cd) {
+        cd(null, Date.now() + path.extname(file.destination))
+    }
+})
+
+const filter = (req, file, cd) => {
+    if(file.mimetype === "image/jpej" || file.mimetype === "image/png") {
+        cd(null, true)
+    } else {
+        cd(null, false)
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter
+})
+
+module.exports = upload
