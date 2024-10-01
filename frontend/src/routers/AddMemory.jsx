d7e9f5a4435216581c3987940bb85ee29caac0ed
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-10-01 14:53:19 -0300

    test

--- frontend/src/routers/AddMemory.jsx
+++ frontend/src/routers/AddMemory.jsx
@@ -5,23 +5,41 @@ import { useState } from "react"
 import "./AddMemory.css"
 
 const AddMemory = () => {
+  const [inputs, setInputs] = useState({})
+  const [image, setImage] = useState(null)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log(inputs, image)
+  }
+
+  const handleChange = (event) => {
+    if (event.target.name === "image") {
+      setImage(event.target.files[0])
+
+    } else {
+      setInputs({ ...inputs, [event.target.name]: event.target.value })
+    }
+  }
+
+
   return (
     <div className="add-memory-page">
       <h2>Crie uma memória</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Título: </p>
-          <input type="text" placeholder="Defina um título" name="title"/>
+          <input type="text" placeholder="Defina um título" name="title" onChange={handleChange} />
         </label>
         <label>
           <p>Descrição: </p>
-          <textarea type="text" placeholder="Explique o que aconteceu..." name="description"></textarea>
+          <textarea type="text" placeholder="Explique o que aconteceu..." name="description" onChange={handleChange}></textarea>
         </label>
         <label>
           <p>Foto: </p>
-          <input type="file" name="image" />
+          <input type="file" name="image" onChange={handleChange} />
         </label>
-        <input type="submit" value="Enviar" className="btn"/>
+        <input type="submit" value="Enviar" className="btn" />
       </form>
     </div>
   )
