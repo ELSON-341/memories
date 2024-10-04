b34e77b03691efe59b8505e57180c14afc0a40f7
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-10-04 14:19:09 -0300

    commit

--- frontend/src/routers/Home.jsx
+++ frontend/src/routers/Home.jsx
@@ -3,15 +3,23 @@ import axios from "../axios/axios-config"
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
+import "./Home.css"
+
 const Home = () => {
   const [memories, setMemories] = useState([])
 
 
   useEffect(() => {
     const getMemories = async () => {
-      const res = await axios.get("/memories")
+      try {
+        const res = await axios.get("/memories")
 
-      setMemories(res.data)
+        setMemories(res.data)
+      } catch (error) {
+        console.log(`Um ${error}`);
+        
+      }
+
     }
     getMemories()
   }, [])
@@ -20,8 +28,12 @@ const Home = () => {
     <div className="home">
       <h2>Confira as últimas memórias</h2>
       <div className="memories-container">
-        {memories.length === 0 && memories.map(memory => (
-          <p>{memory.title}</p>
+        {memories.length > 0 && memories.map((memory) => (
+          <div className="memory" key={memory._id}>
+            <img src={`${axios.defaults.baseURL}${memory.src}`} alt={memory.title} />
+            <p>{memory.title}</p>
+            <Link className="btn" to={`/memories/${memory._id}`}>Conmentar</Link>
+          </div>
         ))}
       </div>
     </div>
